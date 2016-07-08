a1cd7049e02719545d8593a78f79589fd1ac5156
ptasiek3003 <ptasiek3003@gmail.com>
2016-07-08 18:45:25 +0200

    dodano cwieczenie 2

--- nasz-skrypt.js
+++ nasz-skrypt.js
@@ -15,3 +15,23 @@ iPhone6S.printInfo();
 samsung6.printInfo();
 nokia.printInfo();
 
+
+function Button(text) {
+	this.text = text || "HELLO!!";
+}
+
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		$('body').append(this.$element);
+		this.$element.click(function() {
+			alert(self.text);
+		});
+	}
+};
+
+var btn1 = new Button('Hello!');
+btn1.create();
+
