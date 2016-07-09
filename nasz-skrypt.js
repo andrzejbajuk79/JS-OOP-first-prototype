a5a00b7424e3d1a57396341f5ce8f5a49240d6d1
ptasiek3003 <ptasiek3003@gmail.com>
2016-07-09 22:49:08 +0200

    Update nasz-skrypt.js

--- nasz-skrypt.js
+++ nasz-skrypt.js
@@ -20,17 +20,17 @@ function Button(text) {
 	this.text = text || "HELLO!!";
 }
 
-Button.prototype = {
-	create: function() {
-		var self = this;
-		this.$element = $('<button>');
-		this.$element.text(this.text);
-		$('body').append(this.$element);
-		this.$element.click(function() {
-			alert(self.text);
-		});
-	}
+Button.prototype.create =  function() {
+        var self = this;
+        this.$element = $('<button>');
+        this.$element.text(this.text);
+        $('body').append(this.$element);
+        this.$element.click(function() {
+            alert(self.text);
+        });
 };
+var btn1 = new Button('Hello!');
+btn1.create();
 
 var btn1 = new Button('Hello!');
 btn1.create();
