e9423eaeaeaf332de37dfe0ad5056fb99810d900
ptasiek3003 <ptasiek3003@gmail.com>
2016-07-10 18:10:02 +0200

    Update nasz-skrypt.js

--- nasz-skrypt.js
+++ nasz-skrypt.js
@@ -1,19 +1,33 @@
-function Telefon(marka, cena, kolor){
+function Telefon(marka, kolor,cena){
 	this.marka = marka;
 	this.cena = cena;
 	this.color = kolor;
 };
 
-Telefon.prototype.printInfo = function() {
-	document.write("Marka telefonu :" + this.marka + "<br> " + "cena :" + this.cena +"<br>" + "Kolor : " + this.color + "<br>");
-};
 var iPhone6S = new Telefon('Apple', 'srebrny', 2250);
-var samsung6 = new Telefon('Samsung', 'czarny', 2250);
-var nokia = new Telefon('Nokia', 'zielony', 2250);
+var samsung6 = new Telefon('Samsung', 'czarny', 4250);
+var nokia = new Telefon('Nokia', 'zielony', 3250);
+
+Telefon.prototype.calculateWarrantyCost = function() {
+var warrantyCost = this.cena * 0.12;
+document.write("cena gwarancji wynosi :" + warrantyCost + " <br>");
+
+console.log (warrantyCost);
+};
+
+Telefon.prototype.printInfo = function() {
+	document.write("Marka telefonu :" + this.marka + "<br> " + "cena :" + this.cena +"<br>" + "Kolor : " + 
+		this.color + "<br>" );
+};
+
 
 iPhone6S.printInfo();
+iPhone6S.calculateWarrantyCost();
 samsung6.printInfo();
+samsung6.calculateWarrantyCost();
 nokia.printInfo();
+nokia.calculateWarrantyCost();
+
 
 
 function Button(text) {
